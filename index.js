7d19924da232fd50e3cfc4ceb4e4515b5658ffb1
Anshumank_Sahay <sahayanshumank@gmail.com>
2024-08-03 18:06:25 +0530

    Matching colors By Colr Pallete

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ let O = document.querySelector(".player-1");
 let X = document.querySelector(".player-2");
 let winBox = document.querySelector(".winner");
 let resetGame = document.querySelector("#reset");
+let wrapper = document.querySelector(".wrapper");
+// let chance = document.querySelector(".chance");
+// let player = document.querySelector(".player");
 let turnO = true;
 window.myGlobalVar = turnO;
-O.style.backgroundColor = "rgba(37, 56, 34)";
+// O.style.backgroundColor = "rgba(37, 56, 34)";
+O.classList.add("active");
 
 const winPatterns = [
   [0, 1, 2],
@@ -24,15 +28,19 @@ boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnO) {
       box.innerHTML = "O";
-      box.style.color = "rgb(0,255,0)";
-      X.style.backgroundColor = "rgba(37, 56, 34)";
-      O.style.backgroundColor = "transparent";
+      box.style.color = "#e0e1dd";
+      O.classList.remove("active");
+      X.classList.add("active");
+      // O.style.backgroundColor = "transparent";
+      // X.style.backgroundColor = "rgb(52, 165, 165,0.5)";
       turnO = false;
     } else {
       box.innerHTML = "X";
-      box.style.color = "rgb(255, 0, 0)";
-      X.style.backgroundColor = "transparent";
-      O.style.backgroundColor = "rgba(37, 56, 34)";
+      box.style.color = "#e0e1dd";
+      X.classList.remove("active");
+      O.classList.add("active");
+      // X.style.backgroundColor = "transparent";
+      // O.style.backgroundColor = "rgb(52, 165, 165,0.5)";
       turnO = true;
     }
     box.disabled = true;
@@ -48,8 +56,14 @@ const disableBoxes = () => {
 };
 
 showWinner = (winner) => {
+  // winBox.classList.remove("hide");
   winBox.innerHTML = `${winner}  HAS  WON!`;
-  winBox.classList.remove("hide");
+  winBox.classList.add("active-popup");
+  wrapper.classList.add("win");
+  // chance.classList.add("win")
+  // player.classList.add("win")
+  O.classList.add("win");
+  X.classList.add("win");
 };
 
 const checkWinner = () => {
@@ -62,8 +76,10 @@ const checkWinner = () => {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         disableBoxes();
-        X.style.backgroundColor = "transparent";
-        O.style.backgroundColor = "transparent";
+        // X.style.backgroundColor = "transparent";
+        // O.style.backgroundColor = "transparent";
+        X.classList.remove("active");
+        O.classList.remove("active");
         if (pos1Val === "O") {
           turnO = false;
         } else {
@@ -83,14 +99,22 @@ resetGame.addEventListener("click", () => {
     box.disabled = false;
     box.innerHTML = "";
     box.classList.remove("win-bg");
-    winBox.classList.add("hide");
+    // winBox.classList.add("hide");
+    winBox.classList.remove("active-popup");
+    wrapper.classList.remove("win");
+    // chance.classList.remove("win")
+    // player.classList.remove("win")
+    O.classList.remove("win");
+    X.classList.remove("win");
   });
   console.log(turnO);
   if (turnO === true) {
-    O.style.backgroundColor = "rgba(37, 56, 34)";
-    console.log("O");
+    // O.style.backgroundColor = "rgb(52, 165, 165)";
+    O.classList.add("active");
+    // console.log("O");
   } else {
-    X.style.backgroundColor = "rgba(37, 56, 34)";
-    console.log("X");
+    // X.style.backgroundColor = "rgb(52, 165, 165)";
+    X.classList.add("active");
+    // console.log("X");
   }
 });
