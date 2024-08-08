8c055619902cb50a65b32c100eaca5b1fa55f1d8
Anshumank_Sahay <sahayanshumank@gmail.com>
2024-08-08 15:20:23 +0530

    Added Transition & Draw  winner stage

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ let X = document.querySelector(".player-2");
 let winBox = document.querySelector(".winner");
 let resetGame = document.querySelector("#reset");
 let wrapper = document.querySelector(".wrapper");
-// let chance = document.querySelector(".chance");
-// let player = document.querySelector(".player");
 let turnO = true;
-window.myGlobalVar = turnO;
-// O.style.backgroundColor = "rgba(37, 56, 34)";
+let winner = false;
+
 O.classList.add("active");
 
 const winPatterns = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
-
   [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-
   [0, 4, 8],
   [2, 4, 6],
 ];
@@ -31,16 +27,12 @@ boxes.forEach((box) => {
       box.style.color = "#e0e1dd";
       O.classList.remove("active");
       X.classList.add("active");
-      // O.style.backgroundColor = "transparent";
-      // X.style.backgroundColor = "rgb(52, 165, 165,0.5)";
       turnO = false;
     } else {
       box.innerHTML = "X";
       box.style.color = "#e0e1dd";
       X.classList.remove("active");
       O.classList.add("active");
-      // X.style.backgroundColor = "transparent";
-      // O.style.backgroundColor = "rgb(52, 165, 165,0.5)";
       turnO = true;
     }
     box.disabled = true;
@@ -50,20 +42,38 @@ boxes.forEach((box) => {
 });
 
 const disableBoxes = () => {
-  for (let box of boxes) {
+  boxes.forEach((box) => {
     box.disabled = true;
-  }
+    console.log(box.disabled);
+  });
 };
 
-showWinner = (winner) => {
-  // winBox.classList.remove("hide");
-  winBox.innerHTML = `${winner}  HAS  WON!`;
+const showWinner = (winner) => {
+  winBox.innerHTML = `${winner} HAS WON!`;
   winBox.classList.add("active-popup");
   wrapper.classList.add("win");
-  // chance.classList.add("win")
-  // player.classList.add("win")
   O.classList.add("win");
   X.classList.add("win");
+};
+
+const showDraw = () => {
+  winBox.innerHTML = `IT'S A DRAW!`;
+  winBox.classList.add("active-popup");
+  wrapper.classList.add("win");
+  O.classList.add("win");
+  X.classList.add("win");
+};
+
+const checkDraw = () => {
+  let draw = true;
+  boxes.forEach((box) => {
+    if (box.innerHTML === "") {
+      draw = false;
+    }
+  });
+  if (draw && !winner) {
+    showDraw();
+  }
 };
 
 const checkWinner = () => {
@@ -72,26 +82,22 @@ const checkWinner = () => {
     let pos2Val = boxes[patterns[1]].innerHTML;
     let pos3Val = boxes[patterns[2]].innerHTML;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         disableBoxes();
-        // X.style.backgroundColor = "transparent";
-        // O.style.backgroundColor = "transparent";
+        winner = true;
         X.classList.remove("active");
         O.classList.remove("active");
-        if (pos1Val === "O") {
-          turnO = false;
-        } else {
-          turnO = true;
-        }
 
         boxes[patterns[0]].classList.add("win-bg");
         boxes[patterns[1]].classList.add("win-bg");
         boxes[patterns[2]].classList.add("win-bg");
+        return;
       }
     }
   }
+  checkDraw();
 };
 
 resetGame.addEventListener("click", () => {
@@ -99,22 +105,15 @@ resetGame.addEventListener("click", () => {
     box.disabled = false;
     box.innerHTML = "";
     box.classList.remove("win-bg");
-    // winBox.classList.add("hide");
     winBox.classList.remove("active-popup");
     wrapper.classList.remove("win");
-    // chance.classList.remove("win")
-    // player.classList.remove("win")
     O.classList.remove("win");
     X.classList.remove("win");
   });
-  console.log(turnO);
-  if (turnO === true) {
-    // O.style.backgroundColor = "rgb(52, 165, 165)";
+  winner = false;
+  if (turnO) {
     O.classList.add("active");
-    // console.log("O");
   } else {
-    // X.style.backgroundColor = "rgb(52, 165, 165)";
     X.classList.add("active");
-    // console.log("X");
   }
 });
